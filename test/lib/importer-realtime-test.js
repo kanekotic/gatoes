0414e12ff64757de6282b01a8a3f5e67f411e6e7
kanekotic <alvarojosepl@gmail.com>
2018-06-28 07:33:51 +0200

    added import from google analytics

---
+++ test/lib/importer-realtime-test.js
@@ -0,0 +1,26 @@
+jest.mock('../../lib/helpers/google-analytics', () => {
+    return {
+        login: jest.fn(),
+        realtime: jest.fn()
+    }
+})
+
+const importer = require('../../lib/importer-realtime'),
+    analytics = require('../../lib/helpers/google-analytics'),
+    faker = require('faker')
+
+describe('import realtime', () => {
+    it('import from google analytics',async () => {
+        const email = faker.internet.email(),
+            path = `/some/file/path/${faker.random.uuid()}.conf`,
+            jwtClient = { client: faker.random.uuid() },
+            config = { config: faker.random.uuid() }
+
+        analytics.login.mockImplementation(() => jwtClient)
+        
+        await importer(email, path, config)
+
+        expect(analytics.login).toBeCalledWith(email, path)
+        expect(analytics.realtime).toBeCalledWith(jwtClient, config)
+    })
+})
