dc19ddc34ac0cc9cd18def2a4a7e58b4745cf12e
kanekotic <alvarojosepl@gmail.com>
2018-06-28 08:07:56 +0200

    added realtime analytics

--- test/lib/importer-realtime-test.js
+++ test/lib/importer-realtime-test.js
@@ -14,13 +14,16 @@ describe('import realtime', () => {
         const email = faker.internet.email(),
             path = `/some/file/path/${faker.random.uuid()}.conf`,
             jwtClient = { client: faker.random.uuid() },
-            config = { config: faker.random.uuid() }
+            config = { config: faker.random.uuid() },
+            expectedResult = faker.random.uuid()
 
         analytics.login.mockImplementation(() => jwtClient)
+        analytics.realtime.mockImplementation(() => Promise.resolve(expectedResult))
         
-        await importer(email, path, config)
+        let result = await importer(email, path, config)
 
         expect(analytics.login).toBeCalledWith(email, path)
         expect(analytics.realtime).toBeCalledWith(jwtClient, config)
+        expect(result).toEqual(expectedResult)
     })
 })
