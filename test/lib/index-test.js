971e58bf61aa33af19cfa276f3e9a448868f2d01
kanekotic <alvarojosepl@gmail.com>
2018-07-02 09:56:27 +0200

    added output to console

--- test/lib/index-test.js
+++ test/lib/index-test.js
@@ -1,9 +1,12 @@
 jest.mock('../../lib/importer', () => jest.fn())
 jest.mock('../../lib/exporter', () => jest.fn())
+jest.mock('../../lib/exporter', () => jest.fn())
 
-const index = require('../../lib/index')
+const index = require('../../lib/index'),
+    faker = require('faker')
     importer = require('../../lib/importer'),
-    exporter = require('../../lib/exporter')
+    exporter = require('../../lib/exporter'),
+    consoleAdapter = require('../../lib/adapters/console')
 
 describe('index', () => {
     it('exports the importer',async () => {
@@ -11,5 +14,14 @@ describe('index', () => {
     })
     it('exports the exporter',async () => {
         expect(index.exporter).toBe(exporter)
+    })
+    it('exports outputs to console',async () => {
+        const email = faker.internet.email(),
+        path = `/some/file/path/${faker.random.uuid()}.conf`,
+        config = { config: faker.random.uuid() }
+
+        index.outputTo.console(email, path, config)
+        
+        expect(index.exporter).toBeCalledWith(email,path, config, consoleAdapter)
     })
 })
