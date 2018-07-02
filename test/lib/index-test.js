c6fd306d7eb72542fde3c28b4f87d52da799da72
kanekotic <alvarojosepl@gmail.com>
2018-07-02 09:20:05 +0200

    added exporter

--- test/lib/index-test.js
+++ test/lib/index-test.js
@@ -1,10 +1,15 @@
 jest.mock('../../lib/importer', () => jest.fn())
+jest.mock('../../lib/exporter', () => jest.fn())
 
 const index = require('../../lib/index')
-    importer = require('../../lib/importer')
+    importer = require('../../lib/importer'),
+    exporter = require('../../lib/exporter')
 
 describe('index', () => {
     it('exports the importer',async () => {
         expect(index.importer).toBe(importer)
+    })
+    it('exports the exporter',async () => {
+        expect(index.exporter).toBe(exporter)
     })
 })
