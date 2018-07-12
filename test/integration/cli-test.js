f75d4df9f935be917a279e64d154f4dceccc7ad6
kanekotic <alvarojosepl@gmail.com>
2018-07-12 08:39:37 +0200

    added enviroment variables in matrix

--- test/integration/cli-test.js
+++ test/integration/cli-test.js
@@ -3,8 +3,8 @@ const coffee = require('coffee'),
 
 describe('cli', () => {
     it(`should retrieve the metrics from ga and show in console`, done => {
-        let email = process.env.USER_EMAIL
-        let username = process.env.USER_NAME
+        let email = process.env.USER_EMAIL_GATOO
+        let username = process.env.USER_NAME_GATOO
         let expected = '{"reports":[{"columnHeader":{"dimensions":["ga:date"],"metricHeader":{"metricHeaderEntries":[{"name":"ga:users","type":"INTEGER"}]}},"data":{"totals":[{"values":["0"]}],"isDataGolden":true}}]}\n'
         coffee.fork(executable, ['console', email, "../key.pem", username, "2017-06-29","2017-06-30", "ga:users", "ga:date"])
             .expect('stdout', expected)
